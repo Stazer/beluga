89e3bc182b295f61eb80c64590323caff15e00c0
Justus Flerlage <justusflerlage@juteel.com>
2017-04-29 00:41:02 +0200

    replaced std::vector with data_type for tls extensions data

--- include/beluga/tls/tls_extension.hpp
+++ include/beluga/tls/tls_extension.hpp
@@ -8,17 +8,19 @@ namespace beluga
     class tls_extension
     {
     public:
-	tls_extension(std::uint16_t type = 0, const std::vector<std::uint8_t>& data = std::vector<std::uint8_t>());
+	using data_type = std::vector<std::uint8_t>;
+		
+	tls_extension(std::uint16_t type = 0, const data_type& data = data_type());
 
 	void set_type(std::uint16_t type);
 	std::uint16_t get_type() const;
 
-	void set_data(const std::vector<std::uint8_t>& data);
-	const std::vector<std::uint8_t>& get_data() const;
-	std::vector<std::uint8_t>& get_data();
+	void set_data(const data_type& data);
+	const data_type& get_data() const;
+        data_type& get_data();
 
     private:
 	std::uint16_t type;
-	std::vector<std::uint8_t> data;
+        data_type data;
     };
 }
--- source/beluga/tls/tls_extension.cpp
+++ source/beluga/tls/tls_extension.cpp
@@ -1,6 +1,6 @@
 #include <beluga/tls/tls_extension.hpp>
 
-beluga::tls_extension::tls_extension(std::uint16_t type, const std::vector<std::uint8_t>& data):
+beluga::tls_extension::tls_extension(std::uint16_t type, const data_type& data):
     type(type),
     data(data)
 {
@@ -15,15 +15,15 @@ std::uint16_t beluga::tls_extension::get_type() const
     return type;
 }
 
-void beluga::tls_extension::set_data(const std::vector<std::uint8_t>& data)
+void beluga::tls_extension::set_data(const data_type& data)
 {
     this->data = data;
 }
-const std::vector<std::uint8_t>& beluga::tls_extension::get_data() const
+const beluga::tls_extension::data_type& beluga::tls_extension::get_data() const
 {
     return data;
 }
-std::vector<std::uint8_t>& beluga::tls_extension::get_data()
+beluga::tls_extension::data_type& beluga::tls_extension::get_data()
 {
     return data;
 }
